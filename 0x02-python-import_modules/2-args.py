98c43fe97d358dacb78d100c4a8fe905e88f8d21
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-08-25 11:01:44 -0700

    cont

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-l = int(len(sys.argv))
-if l < 2:
-    print("{:d} arguments.".format(l-1))
-elif l == 2:
-    print("{:d} argument:".format(l-1))
-    print("{:d}: {}".format((l-1), (sys.argv[l-1])))
-elif l > 2:
-    print("{:d} arguments:".format(l-1))
-    for i in range(l-1):
-        print("{:d}: {}".format((i+1), (sys.argv[i+1]))) 
+if __name__ == "__main__":
+    import sys
+    l = int(len(sys.argv))
+    if l < 2:
+        print("{:d} arguments.".format(l-1))
+    elif l == 2:
+        print("{:d} argument:\n".format(l-1))
+        print("{:d}: {}".format((l-1), (sys.argv[l-1])))
+    elif l > 2:
+        print("{:d} arguments:\n".format(l-1))
+        for i in range(l-1):
+            print("{:d}: {}".format((i+1), (sys.argv[i+1]))) 
