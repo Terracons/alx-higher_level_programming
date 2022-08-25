a9f0ee99e6d787683a4df7576bd8540638910c39
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-08-25 11:11:58 -0700

    task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    l = int(len(sys.argv))
-    if l < 2:
-        print("{:d} arguments.".format(l-1))
-    elif l == 2:
-        print("{:d} argument:\n".format(l-1))
-        print("{:d}: {}".format((l-1), (sys.argv[l-1])))
-    elif l > 2:
-        print("{:d} arguments:\n".format(l-1))
-        for i in range(l-1):
-            print("{:d}: {}".format((i+1), (sys.argv[i+1]))) 
+    list_n = int(len(sys.argv))
+    if list_n < 2:
+        print("{:d} arguments.".format(list_n-1))
+    elif list_n == 2:
+        print("{:d} argument:".format(list_n-1))
+        print("{:d}: {:s}".format((list_n-1), (sys.argv[list_n-1])))
+    elif list_n > 2:
+        print("{:d} arguments:".format(list_n-1))
+        for i in range(list_n-1):
+            print("{:d}: {:s}".format((i+1), (sys.argv[i+1])))
