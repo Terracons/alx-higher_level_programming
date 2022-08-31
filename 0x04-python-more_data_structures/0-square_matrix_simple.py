20df26017dee2937b8370abff827fe5cb1b22f12
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-08-31 16:56:26 -0700

    python data structure corrected

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def mult_2(mylist):
-    new=[]
+    new = []
     for i in mylist:
         new.append(i**2)
     return new
+
+
 def square_matrix_simple(matrix=[]):
     temp_matrix = matrix.copy()
-    temp_matrix= list(map((lambda x: mult_2(x)), temp_matrix))
+    temp_matrix = list(map((lambda x: mult_2(x)), temp_matrix))
     return temp_matrix
