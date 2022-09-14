b04c86f7097c31cb983c3fdf52cbcb2e0c7ed8d4
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-09-14 16:57:29 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" Square class.
+
+This module contain an empty class
+it can only be use to ppass 
+"""
 class Square:
     """ This is summary of the class square """
     pass
