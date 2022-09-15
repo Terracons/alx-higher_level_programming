6b741a97df3b847975507c5ccf0f35f7d56d20a4
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2022-09-14 17:10:03 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Sqauare class
+"""
+
+
 class Square:
+    """ This is docstrings of
+    class square
+    Attributes:
+       size(int) : Sizeof square
+    """
     def __init__(self, size):
+        """docstring on the __init__ method"""
         self.__size = size
